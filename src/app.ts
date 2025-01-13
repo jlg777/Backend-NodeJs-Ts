ed75252a160ad4f2a999197dc5d90636f35db867
= <=>
2025-01-13 11:55:48 -0300

    Se instala morgan

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from 'express'
 import diariesRouter from './routes/dariariesRoutes'
+import morgan from 'morgan'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(morgan('dev'))
 
 app.get('/', (_req: Request, res: Response) => {
 res.send('Hola')
--- src/routes/dariariesRoutes.ts
+++ src/routes/dariariesRoutes.ts
@@ -1,9 +1,11 @@
 import  express, { Request, Response }  from "express";
+import { getEntries } from "../services/diariesServices";
 
 const diariesRouter = express.Router()
 
 diariesRouter.get('/', (_req:Request, res: Response) => {
-    res.status(201)
+    const entries = getEntries()
+    res.status(201).json(entries)
 })
 
 export default diariesRouter
