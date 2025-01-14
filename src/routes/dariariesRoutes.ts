70190c849dc5738373ce8fdc0bfd48676e79bb22
= <=>
2025-01-14 10:24:13 -0300

    update

--- src/routes/dariariesRoutes.ts
+++ src/routes/dariariesRoutes.ts
@@ -1,11 +1,17 @@
 import express, { Request, Response } from 'express'
-import { getEntries } from '../services/diariesServices'
+import { findId, getEntries } from '../services/diariesServices'
 
 const diariesRouter = express.Router()
 
 diariesRouter.get('/', (_req: Request, res: Response) => {
   const entries = getEntries()
   res.status(201).json(entries)
+})
+
+diariesRouter.get('/:id', (req: Request, res: Response) => {
+  const { id } = req.params
+  const entriesId = findId(Number(id))
+  res.status(201).json(entriesId)
 })
 
 export default diariesRouter
--- src/services/diariesServices.ts
+++ src/services/diariesServices.ts
@@ -3,3 +3,8 @@ import diariesData from '../mock/diariesData.json'
 
 const diaries: diariesEntry[] = diariesData as diariesEntry[]
 export const getEntries = (): diariesEntry[] => diaries
+
+export const findId = (id: Number): diariesEntry | undefined => {
+  const entry = diaries.find(d => d.id === id)
+  return entry
+}
