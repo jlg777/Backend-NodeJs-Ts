b02f8df63509c6e32139d81fd8ad8ceca7615e77
= <=>
2025-01-15 15:38:39 -0300

    se modificaron los service

--- src/services/diariesServices.ts
+++ src/services/diariesServices.ts
@@ -1,17 +1,38 @@
-import { diariesEntry, diarieWithoutComment } from '../types'
+import { diariesEntry } from '../types'
 import diariesData from '../mock/diariesData.json'
+import { Request, Response } from 'express'
 
 const diaries: diariesEntry[] = diariesData as diariesEntry[]
-export const getEntries = (): diariesEntry[] => diaries
 
-export const findId = (id: number): diarieWithoutComment | undefined => {
-  const entry = diaries.find(d => d.id === id)
-
-  if (entry !== undefined) {
-    // Eliminamos la propiedad 'comment' del objeto
-    const { comment, ...entryWithoutComment } = entry
-    return entryWithoutComment
+export const getEntries = (_req: Request, res: Response): void => {
+  try {
+    // const data: diariesEntry[] = diaries
+    res.status(200).json(diaries)
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Error al consultar los cursos:', error)
+      res.status(500).send(error.message)
+    }
   }
+}
 
-  return undefined
+export const findId = (req: Request, res: Response): void => {
+  const { id } = req.params
+
+  try {
+    const entry = diaries.find(d => d.id === Number(id))
+    if (entry == null) {
+      return
+    }
+    const { comment, ...entryWithoutComment } = entry
+    res.status(201).json(entryWithoutComment)
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('Error al consultar los cursos:', error)
+      res.status(500).send(error.message)
+    } else {
+      console.error('Error desconocido:', error)
+      res.status(500).send('Error desconocido')
+    }
+  }
 }
